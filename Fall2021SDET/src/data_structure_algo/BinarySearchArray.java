b181950e9848764a588e5ef02e6c3ddff8faf0e9
alexlukach <oleksandralukach>
2022-02-16 15:41:12 -0800

    new new

---
+++ Fall2021SDET/src/data_structure_algo/BinarySearchArray.java
@@ -0,0 +1,16 @@
+package data_structure_algo;
+
+import javax.xml.crypto.Data;
+import java.text.DateFormat;
+
+public class BinarySearchArray {
+
+    /**
+     * Pseudo code
+     *
+     * split in half
+     */
+    public static void main(String[] args) {
+        //DateFormat
+    }
+}
--- SeleniumFramework/src/main/java/com/class03/DropDown.java
+++ SeleniumFramework/src/main/java/com/class03/DropDown.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import java.util.Iterator;
 import java.util.List;
@@ -30,6 +31,7 @@ public class DropDown {
         Thread.sleep(5000); //wait for 5 sec
         catDropDownSelect.selectByVisibleText("Books"); //text - String
 
+
         // Create a list of all options in dropDown using .getOptions()
         List<WebElement> listOfDropDown = catDropDownSelect.getOptions();
 
@@ -37,10 +39,66 @@ public class DropDown {
         for(WebElement e : listOfDropDown){
             System.out.println(e.getText());
         }
+
+        Assert.assertEquals(listOfDropDown.size(), 36);
+
 //        Iterator<WebElement> it = listOfDropDown.iterator();
 //        while(it.hasNext()){
 //            System.out.println(it.next().getText());
 //        }
 
+        List<WebElement> selectedOptions = catDropDownSelect.getAllSelectedOptions();
+        Assert.assertTrue(selectedOptions.size() == 1 &&
+                selectedOptions.get(0).getText().equals("Books"));
+
+        catDropDownSelect.selectByValue("4");
+
+        selectedOptions = catDropDownSelect.getAllSelectedOptions();
+        Assert.assertTrue(selectedOptions.size() == 1 &&
+                selectedOptions.get(0).getText().equals("Baby"));
+
+        //dropdown.getFirstSelectedOption().getText().equals("hello");
+
+        // Practice multiple select:
+        WebElement selectElement = driver.findElement(By.id(""));
+        Select dropdown = new Select(selectElement);
+
+        //verify is it a multiple select
+        Assert.assertTrue(dropdown.isMultiple()); //true or false
+
+        //what options are selected
+        List<WebElement> selectedOptions2 = dropdown.getAllSelectedOptions();
+
+        //print currently selected options
+        for (WebElement selected : selectedOptions2){
+            System.out.println(selected.getText());
+        }
+        //I want to select Java, Python
+
+        //first I deselect HTML and CSS
+        dropdown.deselectAll();
+        //dropdown.deselectByVisibleText("HTML");
+
+        //now select Java
+        dropdown.selectByIndex(5);
+        //now select Python
+        dropdown.selectByValue("Python");
+
+        //print new selected options
+        for (WebElement selected : selectedOptions2){
+            System.out.println(selected.getText());
+        }
+
+        selectedOptions2 = dropdown.getAllSelectedOptions();
+        Assert.assertEquals(selectedOptions.size(), 2);
+
+
+
+
+
+
+
     }
+
+
 }
